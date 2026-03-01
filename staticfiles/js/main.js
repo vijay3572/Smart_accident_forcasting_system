5f88d48083fcf2310a84147ba78c1c801cd665de
Vijay <your_github_vvijay3572@gmail.com>
2026-03-01 12:27:24 +0530

    ignore static and media folders

--- staticfiles/js/main.js
+++
@@ -1,6 +0,0 @@
-// static/js/main.js
-// small helper: focus first input on load
-document.addEventListener('DOMContentLoaded', function(){
-  const firstInput = document.querySelector('input.form-control');
-  if(firstInput) firstInput.focus();
-});
